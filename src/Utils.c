4fb479752caa608873e4dcf26d2af0dd6df2eb2e
MrBaguette07 <mathsgovlog@gmail.com>
2023-01-11 16:58:37 +0100

    Commentaires Utils

--- src/Utils.c
+++ src/Utils.c
@@ -7,7 +7,9 @@ void initGrille(char grille[3][3])
 {
     /* Initalise les caractères de la grille.*/
     int i,j;
+    /* La ligne */
     for (i = 0; i < LIGNES; i++)
+        /* La colonne */
         for (j = 0; j < COLONNES; j++)
             grille[i][j] = ' ';
 }
@@ -19,7 +21,9 @@ void afficheGrille(char grille[3][3])
      */
     printf("\n");
     int col,ligne;
+    /* La ligne */
     for (ligne = 0 ; ligne<LIGNES ; ligne++){
+        /* La colonne */
         for (col = 0 ; col< COLONNES ; col++){
             printf("%c", grille[ligne][col]);
             if (col != 2) {
@@ -34,27 +38,32 @@ int isInGrille(char grille[3][3], int ligne, int colonne)
 {
     printf("%d,%d",ligne,colonne);
     if ((ligne<0) || (ligne>=LIGNES)){
-        printf("%s","En dehors du tab");
+        /* Il dépasse la ligne*/
         return -1;
     }
     if ((colonne<0) || (colonne>=COLONNES)){
-        printf("%s","En dehors du tab");
+        /* Il dépasse la colonne */
         return -1;
     }
+    /* Oui il peut poser */
     return 1;
 }
 
 int isEmpty(char grille[3][3],int ligne, int colonne){
     if ((ligne == 10) && (colonne == 10)){
+        /* Nombre par défaut */
         return -1;
     } 
     if ((grille[ligne][colonne] != 'X') && (grille[ligne][colonne] != 'O')){
+        /* Il y a quelqu'un a cette emplacement */
         return 1;
     }
+    /* Oui il peut poser */
     return -1;
 }
 
 void updateGrille(char grille[3][3],int ligne,int colonne,char type) {
+    /* Mise à jour de la grille en par un nouveau type */
     grille[ligne][colonne] = type;
 }
 
